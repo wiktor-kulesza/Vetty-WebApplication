c46e81dc6e459aad36ff87a39d6c894516f84acd
Gimli037 <wiktor.kulesza037@gmail.com>
2023-02-27 16:00:11 +0100

    Frontend Feat: Created home view with petlists + modify pet view

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from './home';
+import Navbar from './navbar';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import ModifyPet from './modifyPet';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <div className="App">
+          <Navbar/>
+          <div className="content">
+            <Routes>
+              <Route exact path="/" element={<Home/>}/>
+              <Route path="/create" element={<Home/>}/>
+              <Route path="/forum" element={<Home/>}/>
+              <Route path="/login" element={<Home/>}/>
+              <Route path="/register" element={<Home/>}/>
+              <Route path="/modify/pets/:petId" element={<ModifyPet/>}/>
+            </Routes>
+          </div>
+        </div>
+      </Router>
   );
 }
 
