6a35c1b01fcd6b3c39798abc55d4e99dda85fa44
Gimli037 <wiktor.kulesza037@gmail.com>
2023-03-13 19:56:43 +0100

    Backend Feat: Added Jwt Configuration and filters

---
+++ backend/src/main/java/com/example/security/MyUserDetails.java
@@ -0,0 +1,31 @@
+package com.example.security;
+
+import com.example.entity.User;
+import com.example.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MyUserDetails implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        final User appUser = userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("User '" + email + "' not found"));
+        return org.springframework.security.core.userdetails.User
+                .withUsername(email)
+                .password(appUser.getPassword())
+                .authorities(appUser.getRoles())
+                .accountExpired(false)
+                .accountLocked(false)
+                .credentialsExpired(false)
+                .disabled(false)
+                .build();
+    }
+
+}
