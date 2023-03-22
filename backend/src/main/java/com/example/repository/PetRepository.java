b67dbb7235b106889bc750860463be38aed8ea6b
Gimli037 <wiktor.kulesza037@gmail.com>
2023-03-22 12:11:10 +0100

    Backend Ref: Package name change

--- backend/src/main/java/com/example/repository/PetRepository.java
+++ backend/src/main/java/com/example/repository/PetRepository.java
@@ -1,6 +1,6 @@
 package com.example.repository;
 
-import com.example.entity.Pet;
+import com.example.entity.pet.Pet;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
