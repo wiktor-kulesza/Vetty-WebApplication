7e8a25921c6dddc82f3c1ba24a14df7ebf864cce
Gimli037 <wiktor.kulesza037@gmail.com>
2023-05-06 15:10:07 +0200

    Frontend Feat: developed thread view to have some real data

--- frontend/src/thread.js
+++ frontend/src/thread.js
@@ -1,7 +1,24 @@
-import React, {useState} from 'react';
-import {Accordion, Button, Card, Form, ListGroup} from 'react-bootstrap';
+import {useEffect, useState} from 'react';
+import {useParams} from "react-router-dom";
+import {Accordion, Button, Card, Container, Form, ListGroup} from 'react-bootstrap';
+import useFetch from './proccess_data/use_fetch';
+import * as con from './constants';
+import PetMedicalHistoryResults from './petMedicalHistoryResults';
 
-const ThreadView = () => {
+const Thread = () => {
+    const {threadId} = useParams();
+
+    const [thread, setThread] = useState(null);
+    const {
+        data,
+        error
+    } = useFetch(con.URL + con.API_GET_THREAD_BY_ID + threadId);
+
+    useEffect(() => {
+        setThread(data);
+    }, [data]);
+
+
     const [isMedicalHistoryOpen, setIsMedicalHistoryOpen] = useState(false);
     const [comments, setComments] = useState([]);
     const [newComment, setNewComment] = useState('');
@@ -22,68 +39,76 @@ const ThreadView = () => {
     };
 
     return (
-        <Card>
-            <Card.Body>
-                <Card.Title>Thread Title</Card.Title>
-                <Card.Subtitle className="mb-2 text-muted">Thread Author (email)</Card.Subtitle>
-                <Card.Text>
-                    Thread Content
-                </Card.Text>
-                <Accordion>
-                    <Accordion.Item eventKey="0">
-                        <Accordion.Header>
-                            Medical History
-                        </Accordion.Header>
-                        <Accordion.Body>
-                            <ListGroup>
-                                <ListGroup.Item>
-                                    <strong>Pet:</strong> Pet Name
-                                </ListGroup.Item>
-                                <ListGroup.Item>
-                                    <strong>Date:</strong> Medical History Date
-                                </ListGroup.Item>
-                                <ListGroup.Item>
-                                    <strong>Diagnosis:</strong> Medical History Diagnosis
-                                </ListGroup.Item>
-                                <ListGroup.Item>
-                                    <Button onClick={handleToggleMedicalHistory}>
-                                        {isMedicalHistoryOpen ? 'Hide Description' : 'Show Description'}
-                                    </Button>
-                                </ListGroup.Item>
-                                {isMedicalHistoryOpen && (
+        <Container>
+            {thread && !error && (<Card>
+                <Card.Body>
+                    <Card.Title>{thread.title}</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">By: {thread.authorEmail}</Card.Subtitle>
+                    <Card.Text>
+                        {thread.content}
+                    </Card.Text>
+                    <Accordion>
+                        <Accordion.Item eventKey="0">
+                            <Accordion.Header>
+                                Medical History
+                            </Accordion.Header>
+                            <Accordion.Body>
+                                <ListGroup>
                                     <ListGroup.Item>
-                                        Medical History Description and Results
+                                        <strong>Pet:</strong> {thread.pet.name}
                                     </ListGroup.Item>
-                                )}
-                            </ListGroup>
-                        </Accordion.Body>
-                    </Accordion.Item>
-                </Accordion>
-                <Card.Title className="mt-4">Comments</Card.Title>
-                <ListGroup>
-                    {comments.map((comment, index) => (
-                        <ListGroup.Item key={index}>
-                            {comment}
-                        </ListGroup.Item>
-                    ))}
-                </ListGroup>
-                <Form className="mt-4">
-                    <Form.Group controlId="commentForm">
-                        <Form.Label>Add a comment</Form.Label>
-                        <Form.Control
-                            as="textarea"
-                            rows={3}
-                            value={newComment}
-                            onChange={handleCommentChange}
-                        />
-                    </Form.Group>
-                    <Button variant="primary" onClick={handleAddComment}>
-                        Add Comment
-                    </Button>
-                </Form>
-            </Card.Body>
-        </Card>
+                                    <ListGroup.Item>
+                                        <strong>Date:</strong> {thread.medicalHistory.date}
+                                    </ListGroup.Item>
+                                    <ListGroup.Item>
+                                        <strong>Diagnosis:</strong> {thread.medicalHistory.diagnosis}
+                                    </ListGroup.Item>
+                                    <ListGroup.Item>
+                                        <strong>Details:</strong> {thread.medicalHistory.description}
+                                    </ListGroup.Item>
+                                    <ListGroup.Item>
+                                        <Button onClick={handleToggleMedicalHistory}>
+                                            {isMedicalHistoryOpen ? 'Hide Results' : 'Show Results'}
+                                        </Button>
+                                    </ListGroup.Item>
+                                    {isMedicalHistoryOpen && (
+                                        <Container>
+                                            <ListGroup.Item>
+                                                <PetMedicalHistoryResults results={thread.medicalHistory.results}/>
+                                            </ListGroup.Item>
+                                        </Container>
+                                    )}
+                                </ListGroup>
+                            </Accordion.Body>
+                        </Accordion.Item>
+                    </Accordion>
+                    <Card.Title className="mt-4">Comments</Card.Title>
+                    <ListGroup>
+                        {comments.map((comment, index) => (
+                            <ListGroup.Item key={index}>
+                                {comment}
+                            </ListGroup.Item>
+                        ))}
+                    </ListGroup>
+                    <Form className="mb-4">
+                        <Form.Group controlId="commentForm">
+                            <Form.Label>Add a comment</Form.Label>
+                            <Form.Control
+                                as="textarea"
+                                rows={3}
+                                value={newComment}
+                                onChange={handleCommentChange}
+                            />
+                        </Form.Group>
+                        <Button className="mb-4" variant="primary" onClick={handleAddComment}>
+                            Add Comment
+                        </Button>
+                    </Form>
+                </Card.Body>
+            </Card>)}
+
+        </Container>
     );
 };
 
-export default ThreadView;
+export default Thread;
