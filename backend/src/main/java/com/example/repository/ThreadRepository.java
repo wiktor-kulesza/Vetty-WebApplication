0903da5d7a2d8f582bb61b91fdaddfad01a7f1d7
Gimli037 <wiktor.kulesza037@gmail.com>
2023-05-26 10:49:58 +0200

    Backend Fix: Moved Thread Files

--- backend/src/main/java/com/example/repository/ThreadRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.repository;
-
-import com.example.entity.Thread;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ThreadRepository extends JpaRepository<Thread, Integer> {
-}
