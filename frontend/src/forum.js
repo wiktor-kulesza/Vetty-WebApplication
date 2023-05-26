e3b3e768414adda1e405708f8b9d2e664340e857
Gimli037 <wiktor.kulesza037@gmail.com>
2023-05-26 11:02:16 +0200

    Frontend Feat: Added search criteria feat

--- frontend/src/forum.js
+++ frontend/src/forum.js
@@ -5,19 +5,32 @@ import useFetch from "./proccess_data/use_fetch";
 import * as constants from "./constants";
 import ThreadList from "./threadList";
 
-const breeds = ["Labrador", "Poodle", "Golden Retriever", "Bulldog", "Chihuahua", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-const tags = ["Training", "Health", "Behavior", "Adoption", "Grooming"];
-
 
 function ForumView() {
     const [showSearchCriteria, setShowSearchCriteria] = useState(false);
     const [showAddThread, setShowAddThread] = useState(false);
-    const [species, setSpecies] = useState("");
+
+    const [species, setSpecies] = useState(null);
+
+    const [allBreeds, setAllBreeds] = useState([]);
     const [selectedBreeds, setSelectedBreeds] = useState([]);
-    const [ageRange, setAgeRange] = useState([1, 25]);
-    const [searchTerm, setSearchTerm] = useState("");
-    const [searchResults, setSearchResults] = useState(breeds);
+    const [breedsToShow, setBreedsToShow] = useState(allBreeds);
+    const [breedSearchTerm, setBreedSearchTerm] = useState("");
+
+    const [minAge, setMinAge] = useState(0);
+    const [maxAge, setMaxAge] = useState(25);
+
+    const [selectedTags, setSelectedTags] = useState([]);
+
     const [threads, setThreads] = useState([]);
+
+    const {
+        data: breedsData,
+    } = useFetch(constants.URL + constants.API_GET_ALL_BREEDS);
+
+    const {
+        data: tagsData,
+    } = useFetch(constants.URL + constants.API_GET_ALL_TAGS);
 
     const {
         data: pets,
@@ -28,16 +41,67 @@ function ForumView() {
     } = useFetch(constants.URL + constants.API_GET_ALL_THREADS);
 
     useEffect(() => {
+        console.log("threadsFromServer", threadsFromServer)
         setThreads(threadsFromServer);
     }, [threadsFromServer]);
 
+    useEffect(() => {
+        setAllBreeds(breedsData);
+    }, [breedsData]);
+
+    useEffect(() => {
+        if (species) {
+            setBreedsToShow(allBreeds.filter((breed) => species.includes(breed.type.toLowerCase())));
+        }
+    }, [species]);
+
+    useEffect(() => {
+        setBreedsToShow(allBreeds);
+    }, [allBreeds]);
 
     const handleAddThreadButtonClick = () => {
         setShowAddThread(!showAddThread);
     };
 
-    const handleSearchSubmit = (event) => {
+    const handleSearchSubmit = async (event) => {
+        console.log("HANDLE SEARCH SUBMIT")
         event.preventDefault();
+
+        const selectedBreedsNames = selectedBreeds.map((breed) => breed.name);
+        const selectedTagsNames = selectedTags.map((tag) => tag.value);
+        const selectedSpeciesNames = species ? species : ["dog", "cat"]
+        console.log("selectedBreeds", selectedBreedsNames)
+        console.log("species", species)
+        console.log("minAge", minAge)
+        console.log("maxAge", maxAge)
+        console.log("selectedTags", selectedTagsNames)
+
+        try {
+            // Send a POST request with the form data to URL2 using async/await
+            const response = await fetch(constants.URL + constants.API_SEARCH_THREADS, {
+                method: 'POST',
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem('token')}`,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    species: selectedSpeciesNames,
+                    breeds: (selectedBreedsNames && selectedBreedsNames.length && selectedBreedsNames.length > 0) ? selectedBreedsNames : null,
+                    minAge: minAge,
+                    maxAge: maxAge,
+                    tags: (selectedTagsNames && selectedTagsNames.length && selectedTagsNames.length > 0) ? selectedTagsNames : null,
+                })
+            })
+            const data = await response.json();
+            if (data) {
+                if (response.status === 200) {
+                    // setThreads(data);
+                }
+                console.log("data", data)
+            }
+        } catch (error) {
+            console.error('Failed to search threads:', error);
+        }
         // Handle search form submission
     };
 
@@ -50,11 +114,36 @@ function ForumView() {
     };
 
     const handleSearchChange = (event) => {
-        setSearchTerm(event.target.value);
-        const filteredBreeds = breeds.filter((breed) =>
-            breed.toLowerCase().includes(event.target.value.toLowerCase())
+        setBreedSearchTerm(event.target.value);
+        const tmpFilteredBreeds = allBreeds.filter((breed) =>
+            breed.name.toLowerCase().includes(event.target.value.toLowerCase())
         );
-        setSearchResults(filteredBreeds);
+        console.log("search term", event.target.value.toLowerCase())
+        setBreedsToShow(tmpFilteredBreeds);
+    };
+
+    const handleMinChange = (e) => {
+        const value = parseInt(e.target.value);
+        if (value <= maxAge) {
+            setMinAge(value);
+        }
+    };
+
+    const handleMaxChange = (e) => {
+        const value = parseInt(e.target.value);
+        if (value >= minAge && value <= 25) {
+            setMaxAge(value);
+        }
+    };
+
+    const handleTagClick = (tag) => {
+        console.log("HANDLE TAG CLICK")
+        if (selectedTags.includes(tag)) {
+            console.log("includes")
+            setSelectedTags(selectedTags.filter((selectedTag) => selectedTag.value !== tag.value));
+        } else {
+            setSelectedTags([...selectedTags, tag]);
+        }
     };
 
     const handleThreadAdded = (newThread) => {
@@ -63,12 +152,12 @@ function ForumView() {
     };
 
     return (
-        <Container>
+        <Container className="align-items-center">
             <Row>
                 <h1 className="mb-0">Forum</h1>
             </Row>
             <Row>
-                <div className="d-flex align-items-center mb-3">
+                <Container className="d-flex align-items-center mb-3">
                     <Col>
                         <Button onClick={handleAddThreadButtonClick}>
                             {showAddThread ? "Cancel" : "Add new thread"}
@@ -80,79 +169,109 @@ function ForumView() {
                         {showSearchCriteria ? "Hide Search Criteria" : "Show Search Criteria"}
                     </Button></Col>
 
-                </div>
+                </Container>
             </Row>
-            <div className="mb-3">
+            <Container className="mb-3">
                 {showAddThread && <AddThreadForm authorEmail={localStorage.getItem('userEmail')} pets={pets}
                                                  onThreadAdded={handleThreadAdded}/>}
-            </div>
+            </Container>
             <div className="mb-3">{showSearchCriteria && (
-                <Form onSubmit={handleSearchSubmit}>
-                    <Dropdown className="mb-3">
-                        <Dropdown.Toggle variant="secondary" id="species-dropdown">
-                            {species ? species : "Select species"}
-                        </Dropdown.Toggle>
-                        <Dropdown.Menu>
-                            <Dropdown.Item onClick={() => setSpecies("")}>All species</Dropdown.Item>
-                            <Dropdown.Item onClick={() => setSpecies("dog")}>Dog</Dropdown.Item>
-                            <Dropdown.Item onClick={() => setSpecies("cat")}>Cat</Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown>
-                    <Dropdown className="mb-3">
-                        <Dropdown.Toggle variant="secondary" id="breed-dropdown">
-                            {selectedBreeds.length > 0 ? selectedBreeds.join(", ") : "Select breeds"}
-                        </Dropdown.Toggle>
-                        <Dropdown.Menu>
-                            <Form.Group className="mb-3">
-                                <Form.Label>Breed</Form.Label>
-                                <Form.Control
-                                    type="text"
-                                    placeholder="Enter breed"
-                                    value={searchTerm}
-                                    onChange={handleSearchChange}
-                                />
-                                <div className="scrollable-container">
-                                    {searchResults.map((breed) => (
-                                        <Form.Check
-                                            key={breed}
-                                            type="checkbox"
-                                            label={breed}
-                                            checked={selectedBreeds.includes(breed)}
-                                            onChange={() => handleBreedClick(breed)}
+                <Container className="border p-3 mb-4">
+                    <Form onSubmit={handleSearchSubmit}>
+                        <Form.Label>Search Criteria</Form.Label>
+                        <Row>
+                            <Col className="mb-3">
+                                <Form.Text>Species</Form.Text>
+                                <Dropdown className="mb-3">
+                                    <Dropdown.Toggle variant="secondary" id="species-dropdown">
+                                        {species ? species : "Select species"}
+                                    </Dropdown.Toggle>
+                                    <Dropdown.Menu>
+                                        <Dropdown.Item onClick={() => setSpecies(null)}>All species</Dropdown.Item>
+                                        <Dropdown.Item onClick={() => setSpecies(["dog"])}>Dog</Dropdown.Item>
+                                        <Dropdown.Item onClick={() => setSpecies(["cat"])}>Cat</Dropdown.Item>
+                                    </Dropdown.Menu>
+                                </Dropdown>
+                                <Form.Text>Breeds</Form.Text>
+                                <Dropdown className="mb-3">
+                                    <Dropdown.Toggle variant="secondary" id="breed-dropdown">
+                                        {selectedBreeds.length > 0 ? selectedBreeds.map(breed => {
+                                            return (
+                                                <Col>
+                                                    <span className="text-light">{breed.name + ", "}</span>
+                                                </Col>
+                                            )
+                                        }) : <span className="text-light"> Select breeds </span>}
+                                    </Dropdown.Toggle>
+                                    <Dropdown.Menu>
+                                        <Form.Group className="mb-3">
+                                            <Form.Control
+                                                type="text"
+                                                placeholder="Enter breed"
+                                                value={breedSearchTerm}
+                                                onChange={handleSearchChange}
+                                            />
+                                            <div className="scrollable-container">
+                                                {breedsToShow && breedsToShow.map((breed, index) => (
+                                                    <Form.Check
+                                                        key={index}
+                                                        type="checkbox"
+                                                        label={breed.name}
+                                                        checked={selectedBreeds.includes(breed)}
+                                                        onChange={() => handleBreedClick(breed)}
+                                                    />
+                                                ))}
+                                            </div>
+                                        </Form.Group>
+                                    </Dropdown.Menu>
+                                </Dropdown>
+                                <Form.Group as={Row}>
+                                    <Form.Text>
+                                        Min Age
+                                    </Form.Text>
+                                    <Col sm={2}>
+                                        <Form.Control
+                                            type="number"
+                                            value={minAge}
+                                            onChange={handleMinChange}
+                                            min={0}
+                                            max={maxAge}
                                         />
-                                    ))}
-                                </div>
-                            </Form.Group>
-                        </Dropdown.Menu>
-                    </Dropdown>
-                    <Form.Group className="mb-3">
-                        <Form.Label>Age Range</Form.Label>
-                        <input
-                            type="range"
-                            min="1"
-                            max="25"
-                            value={ageRange[1]}
-                            onChange={(event) => setAgeRange([ageRange[0], parseInt(event.target.value)])}
-                            className="form-range me-2"
-                        />
-                        <span className="me-2">{ageRange[0]} Years -</span>
-                        <span>{ageRange[1]} Years</span>
-                    </Form.Group>
-                    <Form.Group className="mb-3">
-                        <Form.Label>Tags</Form.Label>
-                        <div className="scrollable-container">
-                            {tags.map((tag) => (
-                                <Form.Check
-                                    key={tag}
-                                    type="checkbox"
-                                    id={tag}
-                                    label={tag}
-                                />
-                            ))}
-                        </div>
-                    </Form.Group>
-                    <Button className='float-end' type="submit" variant="primary">Search</Button>
-                </Form>
+                                    </Col>
+                                </Form.Group>
+                                <Form.Group as={Row}>
+                                    <Form.Text>
+                                        Max Age
+                                    </Form.Text>
+                                    <Col sm={2}>
+                                        <Form.Control
+                                            type="number"
+                                            value={maxAge}
+                                            onChange={handleMaxChange}
+                                            min={minAge}
+                                            max={25}
+                                        />
+                                    </Col>
+                                </Form.Group>
+                            </Col>
+                        </Row>
+                        <Form.Group className="mb-3">
+                            <Form.Text>Tags</Form.Text>
+                            <div className="scrollable-container">
+                                {tagsData && tagsData.map((tag, index) => (
+                                    <Form.Check
+                                        key={index}
+                                        type="checkbox"
+                                        id={index}
+                                        label={tag.value}
+                                        onChange={() => handleTagClick(tag)}
+                                    />
+                                ))}
+                            </div>
+                        </Form.Group>
+                        <Button type="submit" variant="primary">Search</Button>
+                    </Form>
+                </Container>
             )}
                 {threads && (
                     <ThreadList threads={threads}/>
